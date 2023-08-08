78f01732fa74d0b4b94da0e26f0096264f92006b
Ibrahim Rijal <greget007.ir@gmail.com>
2023-08-09 01:41:13 +0700

    Fix stuck di login waktu web rame

--- src/SiakAutomation.java
+++ src/SiakAutomation.java
@@ -22,18 +22,17 @@ public class SiakAutomation {
 
     /**
      * Method to simulate the filling of IRS forms with dummy data.
-     *
+     * @deprecated This method is deprecated and is only used for testing purposes.
      * @param driver The WebDriver instance used to interact with the web application.
      */
-    private static void dummyChecker(WebDriver driver){
-
+    private static boolean dummyChecker(WebDriver driver) {
         driver.navigate().to("file:///Users/rijal/Documents/SIAK%20Automation/Simulasi%20IRS/Pengisian%20IRS.htm");
         String[][] matkul = {
                 {"673415-3", "IOT 2"},
                 {"672866-4", "Kemjar 1"},
                 {"672882-3", "Probstok 2"},
         };
-        //Dummy Page
+
         try {
             while (true) {
                 Actions mouse = new Actions(driver);
@@ -49,7 +48,7 @@ public class SiakAutomation {
                 List<WebElement> successMsg = driver.findElements(By.xpath("//h3[text()='IRS berhasil tersimpan!']"));
                 if (!successMsg.isEmpty()) {
                     System.out.println("IRS berhasil tersimpan!");
-                    break; // Stop the loop since the IRS is successfully saved
+                    return true; // IRS process successful
                 } else {
                     System.out.println("IRS belum tersimpan. Melanjutkan pengisian IRS kembali.");
                     // ... Continue filling the form again ...
@@ -59,8 +58,8 @@ public class SiakAutomation {
             System.out.println(e);
         }
 
+        return false; // IRS process not successful
     }
-
     /**
      * Reads the MatkulData from a JSON file and returns a list of MatkulData objects.
      *
@@ -98,12 +97,101 @@ public class SiakAutomation {
         return properties;
     }
 
+    /**
+     * Login to the SIAK NG web application.
+     * @param driver The WebDriver instance used to interact with the web application.
+     * @param username The username to login with.
+     * @param password The password to login with.
+     */
+    public static boolean login(WebDriver driver, String username, String password) {
+        try {
+            driver.navigate().to("https://academic.ui.ac.id");
+            Thread.sleep(50);
+            driver.findElement(By.name("u")).sendKeys(username);
+            Thread.sleep(50);
+            driver.findElement(By.name("p")).sendKeys(password);
+            Thread.sleep(50);
+            driver.findElement(By.name("p")).sendKeys(Keys.RETURN);
+            Thread.sleep(3000); // Adjust the wait time as needed
+            WebElement titleElement = driver.findElement(By.tagName("title"));
+            String pageTitle = titleElement.getAttribute("innerHTML");
 
+            if (pageTitle.equals("Selamat Datang - SIAK NG")) {
+                System.out.println("Login successful");
+                return true;
+            }
+
+        } catch (Exception e) {
+            System.out.println("Login error");
+            System.out.println(e);
+        }
+        return false;
+    }
+
+    /** Performs the IRS process.
+     * @param driver The WebDriver instance used to interact with the web application.
+     * @param retry The number of times to retry the IRS process if it fails.
+     * @return True if the IRS process is successful, false otherwise.
+     */
+    public static boolean performIRSProcess(WebDriver driver, int retry, List<MatkulData> matkulList) {
+        try {
+            driver.navigate().to("https://academic.ui.ac.id/main/CoursePlan/CoursePlanEdit");
+            Actions mouse = new Actions(driver);
+            WebElement IrsDropdown = driver.findElement(By.xpath("//*[text()='IRS']"));
+            mouse.moveToElement(IrsDropdown).perform();
+            WebElement IrsMenu = driver.findElement(By.xpath("//*[text()='Isi/Ubah IRS']"));
+            mouse.moveToElement(IrsMenu).click().perform();
+            System.out.println("masuk edit");
+
+            // Rest of the code for IRS process ...
+            for (MatkulData m : matkulList) {
+
+                Thread.sleep(50);
+                driver.findElement(By.cssSelector("input[value='" + m.code + "']")).click();
+                System.out.println("Nama matkul: " + m.name);
+            }
+            Thread.sleep(250);
+            driver.findElement(By.name("submit")).click();
+            Thread.sleep(500);
+
+            List<WebElement> successMsg = driver.findElements(By.xpath("//h3[text()='IRS berhasil tersimpan!']"));
+            if (!successMsg.isEmpty()) {
+                System.out.println("IRS berhasil tersimpan!");
+                System.out.println("Jumlah retry: " + retry);
+                return true; // IRS process successful
+            } else {
+                System.out.println("IRS belum tersimpan. Melanjutkan pengisian IRS kembali.");
+                retry++;
+                // ... Continue filling the form again ...
+            }
+        } catch (Exception e) {
+            List<WebElement> successMsg = driver.findElements(By.xpath("//h3[text()='IRS berhasil tersimpan!']"));
+            System.out.println(successMsg);
+            if (!successMsg.isEmpty()) {
+                System.out.println("IRS berhasil tersimpan!");
+                System.out.println("Jumlah retry: " + retry);
+                driver.navigate().to("https://academic.ui.ac.id/main/CoursePlan/CoursePlanViewSummary");
+                return true; // IRS process successful
+            } else {
+                System.out.println("IRS belum tersimpan. Melanjutkan pengisian IRS kembali.");
+                System.out.println(e);
+                retry++;
+            }
+        }
+        return false; // IRS process not successful
+    }
+
+
+
+    /** Main method to run the program.
+     *
+     * @param args
+     * @throws InterruptedException
+     */
     public static void main(String[] args) throws InterruptedException {
-        int i;
         int retry = 0;
         System.setProperty("webdriver.firefox.whitelistedIps", "0.0.0.0");
-        // Instantiate a SafariDriver class.
+        // Instantiate a FirefoxDriver class.
         WebDriver driver = new FirefoxDriver();
         // Launch Website
 
@@ -121,74 +209,25 @@ public class SiakAutomation {
         List<MatkulData> matkulList = readMatkulDataFromFile(jsonFilePath);
 
 
-        for(i = 0; i<1; i++) {
-            try{
-                driver.navigate().to("https://academic.ui.ac.id");
-                Thread.sleep(50);
-                driver.findElement(By.name("u")).sendKeys(username);
-                Thread.sleep(50);
-                driver.findElement(By.name("p")).sendKeys(password);
-                Thread.sleep(50);
-                driver.findElement(By.name("p")).sendKeys(Keys.RETURN);
-            } catch (Exception e) {
-                i = 0;
-                System.out.println("Login error");
-                System.out.println(e);
-                driver.navigate().to("https://academic.ui.ac.id");
-
+        /*=====================================LOGIN=================================================*/
+        while (true) {
+            if (login(driver, username, password)) {
+                break; // Exit the loop if login is successful
             }
         }
         Thread.sleep(500);
-//        dummyChecker(driver);
-        /*=====================================ISI MK =================================================*/
-        Thread.sleep(500);
+
+        /*=====================================ISI MK=================================================*/
         driver.navigate().to("https://academic.ui.ac.id/main/CoursePlan/CoursePlanEdit");
 
-        while(true) {
+        while (true) {
             System.out.println("Retry ke-" + retry);
-            try{
-                driver.navigate().to("https://academic.ui.ac.id/main/CoursePlan/CoursePlanEdit");
-                Actions mouse = new Actions(driver);
-                WebElement IrsDropdown = driver.findElement(By.xpath("//*[text()='IRS']"));
-                mouse.moveToElement(IrsDropdown).perform();
-                WebElement IrsMenu = driver.findElement(By.xpath("//*[text()='Isi/Ubah IRS']"));
-                mouse.moveToElement(IrsMenu).click().perform();
-                System.out.println("masuk edit");
-                for (MatkulData m : matkulList) {
-                    Thread.sleep(500);
-                    driver.findElement(By.cssSelector("input[value='" + m.code + "']")).click();
-                    System.out.println("Nama matkul: " + m.name);
-                }
-                Thread.sleep(500);
-                driver.findElement(By.name("submit")).click();
-                Thread.sleep(3000);
-                List<WebElement> successMsg = driver.findElements(By.xpath("//h3[text()='IRS berhasil tersimpan!']"));
-                if (!successMsg.isEmpty()) {
-                    System.out.println("IRS berhasil tersimpan!");
-                    System.out.println("Jumlah retry: " + retry);
-                    break; // Stop the loop since the IRS is successfully saved
-                } else {
-                    System.out.println("IRS belum tersimpan. Melanjutkan pengisian IRS kembali.");
-                    retry++;
-                    // ... Continue filling the form again ...
-                }
-
-            } catch (Exception e) {
-                List<WebElement> successMsg = driver.findElements(By.xpath("//h3[text()='IRS berhasil tersimpan!']"));
-                System.out.println(successMsg);
-                if (!successMsg.isEmpty()) {
-                    System.out.println("IRS berhasil tersimpan!");
-                    System.out.println("Jumlah retry: " + retry);
-                    driver.navigate().to("https://academic.ui.ac.id/main/CoursePlan/CoursePlanViewSummary");
-                    break; // Stop the loop since the IRS is successfully saved
-                } else {
-                    System.out.println("IRS belum tersimpan. Melanjutkan pengisian IRS kembali.");
-                    System.out.println(e);
-                    retry++;
-                }
-
+            if (performIRSProcess(driver, retry, matkulList)) {
+                break; // Exit the loop if the IRS process is successful
             }
+
         }
+
 
     }
 }
